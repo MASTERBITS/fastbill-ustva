fab3ccbd7fd64a9a16b8d444a5168a240ed34e2d
Christopher Kreft <email@christopherkreft.de>
2020-12-20 13:07:21 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # fastbill-ustva
-FastBill-UStVA is a Python script to get your preliminary tax report (Umsatzsteuervoranmeldung) done really quick using FastBill
+
+FastBill-UStVA is a Python script to get your preliminary tax report (Umsatzsteuervoranmeldung) done really quick using FastBill. 
+
+It queries the FastBill API for revenues and expenses for the last and the current year and separates the output based on quarters by payment date.
+
+Assumptions:
+
+  - Based on payment date (Ist-Besteuerung)
+  - Quarterly reports
+  - Tax rate for outgoing invoices needs to be manually determined (usually 19%)
+
+
+## Installation
+
+Just clone the git folder and make sure that you set your FastBill API Credentials using Environment Variables:
+ 
+
+```bash
+export API_USER=your@email.com API_KEY=yourapikeynotyourpasswordcheckfastbillsettings
+```
+
+## Usage
+
+```python
+python3 main.py
+```
+
+```python
+Einnahmen 2020 Q1:  1337
+Vorsteuer 2020 Q1: 137.37
+------------------------------------------------------------
+Einnahmen 2020 Q2:  1337
+Vorsteuer 2020 Q2: 137.37
+------------------------------------------------------------
+Einnahmen 2020 Q3:  1337
+Vorsteuer 2020 Q3: 137.37
+------------------------------------------------------------
+Einnahmen 2020 Q4:  1337
+Vorsteuer 2020 Q4: 137.37
+------------------------------------------------------------
+Einnahmen 2019 Q1:  1337
+Vorsteuer 2019 Q1: 137.37
+------------------------------------------------------------
+Einnahmen 2019 Q2:  1337
+Vorsteuer 2019 Q2: 137.37
+------------------------------------------------------------
+Einnahmen 2019 Q3:  1337
+Vorsteuer 2019 Q3: 137.37
+------------------------------------------------------------
+Einnahmen 2019 Q4:  1337
+Vorsteuer 2019 Q4: 137.37
+------------------------------------------------------------
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+from os import environ
+
+
+class FastBillAPIController:
+    def __init__(self, api_username, api_key, api_url='https://my.fastbill.com/api/1.0/api.php'):
+        self.api_username = api_username
+        self.api_key = api_key
+        self.api_url = api_url
+
+    @staticmethod
+    def get_dates(start_month, end_month, year):
+        return tuple(f'{year}-{month:02}-' for month in range(start_month, end_month + 1))
+
+    def _post(self, payload, offset=0, results=None):
+        all_results = results if results else []
+        payload['OFFSET'] = offset
+        payload['LIMIT'] = 100
+        r = requests.post(url=self.api_url, auth=(self.api_username, self.api_key), json=payload)
+        this_result = r.json()
+        this_response = next(
+            iter(this_result['RESPONSE'].values()))  # result has only one response in dict, get the first / only one
+        all_results.extend(this_response)
+        if len(this_response) >= payload['LIMIT']:
+            self._post(payload, offset + payload['LIMIT'], results=all_results)
+        #  pprint(all_results)
+        return all_results
+
+    def _get_service(self, service, year, month=None):
+        payload = {'SERVICE': service,
+                   'Filter': {}
+                   }
+        payload['Filter']['year'] = year
+        if month:
+            payload['Filter']['month'] = month
+        return self._post(payload=payload)
+
+    def get_expenses(self, year, month=None):
+        return self._get_service(service='expense.get', year=year, month=month)
+
+    def get_revenues(self, year, month=None):
+        return self._get_service(service='revenue.get', year=year, month=month)
+
+
+def main():
+    from datetime import datetime
+    now = datetime.now()
+    fbc = FastBillAPIController(api_username=environ.get('API_USER'), api_key=environ.get('API_KEY'))
+
+    revenues = []
+    expenses = []
+    for year in (now.year, now.year - 1):
+        revenues.extend(fbc.get_revenues(year=year))
+        expenses.extend(fbc.get_expenses(year=year))
+
+    for year in (now.year, now.year - 1):
+        for q, i in enumerate(range(0, 12, 3)):
+            print(f'Einnahmen {year} Q{q + 1}: ', round(sum(item['SUB_TOTAL']
+                                                            for item in revenues
+                                                            if item['PAID_DATE'].startswith(
+                fbc.get_dates(start_month=i + 1, end_month=i + 3, year=year)))))
+
+            print(f'Vorsteuer {year} Q{q + 1}:', round(sum(item['VAT_TOTAL']
+                                                           for item in expenses
+                                                           if item['PAID_DATE'].startswith(
+                fbc.get_dates(start_month=i + 1, end_month=i + 3, year=year))), 2))
+            print('-' * 60)
+
+
+if __name__ == '__main__':
+    main()
